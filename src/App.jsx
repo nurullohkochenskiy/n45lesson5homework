d48dd4ac8e0e8c6ea734f05b249ae5fab5894b15
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-04-04 23:47:28 +0500

    going

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Dashboard/>
     </>
   );
 }
