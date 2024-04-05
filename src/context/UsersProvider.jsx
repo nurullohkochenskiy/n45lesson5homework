eb6c1e5df8ed586afa6493b5725aecfedc21c3dc
nurullohkochenskiy <serioskiller2021@gmail.com>
2024-04-05 15:41:27 +0500

    improve

--- src/context/UsersProvider.jsx
+++ src/context/UsersProvider.jsx
@@ -3,18 +3,27 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const UsersContext = createContext();
 
 export function UsersProvider({ children }) {
-  const [teachers, setTeachers] = useState([]);
-  const [students, setStudents] = useState([]);
+  const [teachers, setTeachers] = useState(
+    JSON.parse(localStorage.getItem("teachers")) || []
+  );
+  const [students, setStudents] = useState(
+    JSON.parse(localStorage.getItem("students")) || []
+  );
 
   useEffect(() => {
-    fetch("http://localhost:3001/teachers")
-      .then((response) => response.json())
-      .then((data) => setTeachers(data));
-    fetch("http://localhost:3001/students")
-      .then((response) => response.json())
-      .then((data) => setStudents(data));
+    if (!teachers.length) {
+      fetch("http://localhost:3001/teachers")
+        .then((response) => response.json())
+        .then((data) => setTeachers(data));
+    }
+    if (!students.length) {
+      fetch("http://localhost:3001/students")
+        .then((response) => response.json())
+        .then((data) => setStudents(data));
+    }
   }, []);
-
+  localStorage.setItem("teachers", JSON.stringify(teachers));
+  localStorage.setItem("students", JSON.stringify(students));
   return (
     <UsersContext.Provider value={{ teachers, students }}>
       {children}
--- src/pages/Dashboard/Main.jsx
+++ src/pages/Dashboard/Main.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Dashboard from "../../components/Dashboard";
-
+import { Flex, Typography } from "antd";
+import { useAuth } from "../../components/Auth";
 const Main = () => {
-  return <Dashboard></Dashboard>;
+  const { user } = useAuth();
+  return (
+    <Dashboard>
+      <Typography.Title style={{ color: "white" }} level={4}>
+        Weclome {user.username}
+      </Typography.Title>
+      
+    </Dashboard>
+  );
 };
 
 export default Main;
